49a8e644f60b7c07fca4104db84c6fb72cd3d98b
akyt2 <alfred.truong@gmail.com>
2024-05-25 10:25:49 +0800

    clean up review url building

--- build_review_urls.py
+++ build_review_urls.py
@@ -1,38 +1,55 @@
+#%%
 import pandas as pd
+from pathlib import Path
+import glob
+import os
+import math
+import csv
+#################### COMBINE ALL RESTAURANTS
+ALL_RESTAURANTS_DIR = 'assets/restaurants/'
+ALL_RESTAURANTS_OUT = 'assets/all_restaurants.csv'
 
-df_master = pd.read_csv("assets/data/master_restaurant_urls_AT.csv")
+csv_files = glob.glob(os.path.join(ALL_RESTAURANTS_DIR, "*.csv")) 
+print(csv_files)
+#%%
 
-for i in df['Review URL']:
-    # https://stackoverflow.com/questions/30944577/check-if-string-is-in-a-pandas-dataframe
-    if df_master['Review URL'].str.contains(i).any():
-        # https://stackoverflow.com/questions/36684013/extract-column-value-based-on-another-column-pandas-dataframe
-        df_master.loc[df_master['Review URL'] == i, 'Smile'] = df.loc[df['Review URL'] == i, 'Smile'].iloc[0]
-        df_master.loc[df_master['Review URL'] == i, 'Cry'] = df.loc[df['Review URL'] == i, 'Cry'].iloc[0]
-        df_master.loc[df_master['Review URL'] == i, 'Overall Score'] = df.loc[df['Review URL'] == i, 'Overall Score'].iloc[0]
-        df_master.loc[df_master['Review URL'] == i, 'Bookmark Count'] = df.loc[df['Review URL'] == i, 'Bookmark Count'].iloc[0]
-        df_master.loc[df_master['Review URL'] == i, 'Review Count'] = df.loc[df['Review URL'] == i, 'Review Count'].iloc[0]
-        
-        # If there's duplicate, reduce the amount of duplicate reviews to scrape
-        df.loc[df['Review URL'] == i, 'Review Count'] = df.loc[df['Review URL'] == i, 'Review Count'].iloc[0] - df_master.loc[df_master['Review URL'] == i, 'Review Count'].iloc[0]
-    else:
-        # https://stackoverflow.com/questions/39815646/pandas-append-dataframe-to-another-df
-        df_master = df_master.append(df.loc[df['Review URL'] == i],ignore_index=True)
 
-df_master.to_csv('assets/data/master_restaurant_urls.csv',encoding='utf-8',index=False)
+# Create an empty dataframe to store the combined data
+all_restaurants_df = pd.DataFrame()
+
+# Loop through each CSV file and append its contents to the combined dataframe
+for csv_file in csv_files:
+    df = pd.read_csv(csv_file)
+    print(csv_file,len(df))
+    all_restaurants_df = pd.concat([all_restaurants_df, df])
+
+# combine and save
+all_restaurants_df.drop_duplicates()
+all_restaurants_df.to_csv(ALL_RESTAURANTS_OUT,encoding='utf-8-sig',index=False)
+
+#################### COMBINE ALL RESTAURANTS
+ALL_REVIEW_URLS_OUT = 'assets/all_review_urls.csv'
+
+#%%
+# generate all review subpages
+all_review_urls = []
+for idx,row in all_restaurants_df.iterrows():
+    num_of_review_pages = math.ceil(row['review_count'] / 15) # there are 15 reviews per page
+    
+    for i in range(1,int(num_of_review_pages)):
+        all_review_urls.append(f"https://www.openrice.com{row['review_url']}?page={i}")
+
+# write all urls
+with open(ALL_REVIEW_URLS_OUT, 'w', newline='') as f:
+    writer = csv.writer(f)
+    for review_url in all_review_urls:
+        writer.writerow([review_url])
+#%%
 
 
 #%%
-# generate urls to all review subpages
-review_urls = []
-
-for k in range(len(df['Review Count'])):
-    num_of_review_pages = math.ceil(df['Review Count'][k] / 15.0) # there are 15 reviews per page
-
-    for i in range(1,int(num_of_review_pages)):
-        review_urls.append('https://www.openrice.com'+ str(df['Review URL'][k]) + '?page='+ str( i ))
-
 # print review_urls
-print("There is {} urls to scrape".format(len(review_urls)))
+print("There is {} urls to scrape".format(len(all_review_urls)))
 
 #https://www.openrice.com/en/hongkong/r-burgeroom-causeway-bay-american-hamburger-r189507/reviews
 #https://www.openrice.com/en/hongkong/r-burgeroom-causeway-bay-american-hamburger-r189507/reviews?page=2
